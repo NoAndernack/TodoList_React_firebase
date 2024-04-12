d4a92ee5954b4041a7fdb92196f96637ade4fed7
NoAndernack <andernackn@gmail.com>
2024-04-12 09:50:15 +0200

    starting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+ <div className='min-h-screen flex items-center justify-center'>
+  <div className='bg-gray-100 p-6 rounded shadow-md w-full'>
+  App
+  </div>
+
+  </div>
+  )
+}
+
+export default App
